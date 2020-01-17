67b734a0ac1d555e2fbdc11168b5a8a2eb158715
Kévin Bégasse <kevinbegasse@gmail.com>
2020-01-17 15:24:09 +0100

    WIP isolated the 12 o'clock forecast in order to present it on the front page, only able to access forcast for the five days to come

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -35,7 +35,16 @@ interface DailyForecast {
         humidity: Number,
         temp_kf: Number,
     }
+    dt_txt: string,
+    weather: Array<Weather>,
 
+}
+
+interface Weather {
+    description: string,
+    icon: string,
+    id: Number,
+    main: string
 }
 
 let baseCity : APIDatas;
@@ -46,7 +55,8 @@ const Main: React.FC = ({}) => {
     const[cityForcast, setCityForcast] = useState(baseCity);
     useEffect(() => {
         console.log('on lance fetch');
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?cnt=7&units=metric&id=2983989&APPID=d02c19588f3d2c2f36afd7891ffbba44`)
+        //Todo => Replace API KEY by process.env.REACT_APP_API_KEY
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?units=metric&id=2983990&APPID=d02c19588f3d2c2f36afd7891ffbba44`)
             .then(res => {
                 if(res.status !==200) {
                     setError(true);
@@ -64,15 +74,41 @@ const Main: React.FC = ({}) => {
     )
     
 
-    let currentCity = cityForcast;
+    let currentCity: APIDatas = cityForcast;
+    // To convert the date from string to local date time to sort the date (from the data) first.
+    const options: Object = {weekday: "long", year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+    function formatedDate(dateToFormate :number) : string {
+        return new Date(dateToFormate).toLocaleDateString(undefined, options);
+    }
+    //Function to obtain the hours of each datas in order to show the weather forecast at 12 for the days of the week.
+    function midDayForecast( dt : string) : number {
+        let baseDate = new Date(dt);
+        return baseDate.getHours();
+    }
+    
+    //Seconds in 24h : 86400000
     return (
         <div>
     {loader && 
     <div>
-        <div>Bienvenue à {currentCity.city.name} {console.log(`dans le render`, currentCity)}, la température actuelle est de : {currentCity.list[0].main.temp} °C </div>
-            <div>{currentCity.list.map( day => {
+        <div>Bienvenue à {currentCity.city.name} , la température actuelle est de : {currentCity.list[0].main.temp} °C nous sommes le {new Date().toLocaleDateString(undefined, options)}</div>
+            <div>{
+               
+                // midDayTab.list.filter(day => 
+                //     new Date Date.parse(dt_txt).getHours === 12)
+            
+            currentCity.list.map( (day, index) => {
                console.log('map', day)
-               return (<p>{day.main.temp}</p>)
+               if (midDayForecast(day.dt_txt) === 12){
+               
+               
+               return (
+                   <div key={index}>
+                        <p>{day.main.temp}, {midDayForecast(day.dt_txt)}</p>
+               <p>{formatedDate(Date.parse(day.dt_txt))} ciel : {day.weather[0].description} </p>
+                    </div>
+               )
+               }
                 })
             }</div>
             </div>
