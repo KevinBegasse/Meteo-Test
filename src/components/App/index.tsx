cdd62d43fc1c00832080294789daeb99cf007ea9
Kévin Bégasse <kevinbegasse@gmail.com>
2020-01-28 10:39:28 +0100

    import hookrouter and creation of a 404 component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
+//Importing router
+import { useRoutes, A } from 'hookrouter';
+
 //local imports
 import './App.css';
 import Header from '../Header';
 import Main from '../Main';
+import NotFoundPage from '../NotFoundPage';
 
-
+const routes: {} = {
+  '/' : () => <Main />
+} 
 
 export const App: React.FC = () => {
+  const routeResult = useRoutes(routes);
+
   return (
     <div className="App">
       <Header />
-      <Main/>
+      <A href="/">Rennes</A> <br />
+      {routeResult || <NotFoundPage /> }
+
     </div>
   );
 }
